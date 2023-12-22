be4c9acb5cd90c0c3f4218162c113a5d28949336
Valentin Debarnot <valentin.debarnot@gmail.com>
2023-12-22 15:38:50 +0100

    add install file

--- install_ICETIDE.sh
+++ install_ICETIDE.sh
@@ -2,14 +2,14 @@ conda create --name ice-tide -y python=3.8
 conda activate ice-tide
 python -m pip install --upgrade pip
 pip install torch==2.0.1+cu117 torchvision==0.15.2+cu117 --extra-index-url https://download.pytorch.org/whl/cu117
-conda install -c "nvidia/label/cuda-11.7.1" cuda-toolkit
+conda install -y -c "nvidia/label/cuda-11.7.1" cuda-toolkit
 pip install ninja git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
 pip install odl==0.7
-conda install ipython
-conda install scikit-image
-conda install -c jmcmurray os
-conda install -c conda-forge mrcfile
-conda install matplotlib
+conda install -y ipython
+conda install -y scikit-image
+conda install -y -c jmcmurray os
+conda install -y -c conda-forge mrcfile
+conda install -y matplotlib
 pip install numexpr
-conda install -c conda-forge ml-collections
-conda install -c conda-forge ipdb
+conda install -y -c conda-forge ml-collections
+conda install -y -c conda-forge ipdb
