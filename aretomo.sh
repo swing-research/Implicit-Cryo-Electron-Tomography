bf51eb9676c4f8dec174a8dbe624633a01f006de
ValentinDebarnot <valentin.debarnot@gmail.com>
2024-01-04 18:02:16 +0100

    update comparison

--- aretomo.sh
+++ aretomo.sh
@@ -5,12 +5,14 @@ if [ "$#" -eq 0 ]; then
     path_exp=./results/model_0_SNR_10_size_512_Nangles_61/
     VolZ=0
     AlignZ=180
-    npatch=0
-    echo $npatch
+    npatch=1
 else
     IFS='|' read -r path_aretomo path_exp VolZ AlignZ npatch
-
+    echo $path_aretomo
+    echo $path_exp
+    echo $VolZ
     echo $AlignZ
+    echo $npatch
 fi
 
 
@@ -23,5 +25,3 @@ mkdir -p "$path_save"
 # # FBP with AreTomo
 # $path_aretomo -InMrc "${path_exp}projections.mrc" -OutMrc "${path_save}projections_rec_aretomo_${npatch}by$npatch.mrc"  -VolZ $VolZ -AlignZ 0 -OutBin 1 -AngFile "${path_exp}angles.txt" -AlnFile "${path_save}projections_${npatch}by$npatch.aln" -Wbp 1 -Patch $npatch $npatch -OutImod 1 -DarkTol 0.000001 -FlipVol 1 -TiltAxis 0.00000001 1 -TiltCor -1
 
-
-echo $npatch
