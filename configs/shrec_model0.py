429d85738332804ee1fdf39d546cbbdbc83767d6
ValentinDebarnot <valentin.debarnot@gmail.com>
2024-01-17 17:36:08 +0100

    all models and all snr ready to run for long

--- configs/shrec_model0.py
+++ configs/shrec_model0.py
@@ -72,8 +72,8 @@ def get_config():
     config.Ntest = 100 # number of epoch before display
     config.save_volume = True # saving the volume or not during training
     config.compute_fsc = True # save fsc, takes more time
-    config.scheduler_step_size = 200
-    config.scheduler_gamma = 0.6
+    config.scheduler_step_size = 100
+    config.scheduler_gamma = 0.5
 
     # Sampling strategy
     config.batch_size = 4 # number of viewing direction per iteration
