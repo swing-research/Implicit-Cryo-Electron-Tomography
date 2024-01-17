1462f293f2c4b5375bfb9d4709a4e461197a72f6
ValentinDebarnot <valentin.debarnot@gmail.com>
2024-01-17 13:46:37 +0100

    update multi volues

--- experiment_scripts/simulation_SNR_influence.py
+++ experiment_scripts/simulation_SNR_influence.py
@@ -64,8 +64,9 @@ def main():
         resolution0143 = np.zeros((6,len(volume_name_list)))
         x_val = np.zeros(len(volume_name_list))
         for i, v_SNR in enumerate(v_SNR_list):
-            config.path_save_data = "./results/all_models_"+str(config.volume_name)+"_SNR_"+str(config.SNR_value)+"_size_"+str(config.n1)+"_Nangles_"+str(config.Nangles)+"/"
-            config.path_save = "./results/all_models_"+str(config.volume_name)+"_SNR_"+str(config.SNR_value)+"_size_"+str(config.n1)+"_Nangles_"+str(config.Nangles)+"/"
+            config.SNR_value = v_SNR
+            config.path_save_data = "./results/SNR_exp_"+str(config.volume_name)+"_SNR_"+str(config.SNR_value)+"_size_"+str(config.n1)+"_Nangles_"+str(config.Nangles)+"/"
+            config.path_save = "./results/SNR_exp_"+str(config.volume_name)+"_SNR_"+str(config.SNR_value)+"_size_"+str(config.n1)+"_Nangles_"+str(config.Nangles)+"/"
 
             data_path = os.path.join(config.path_save,'evaluation','FSC.csv')
             fsc = pd.read_csv(data_path+'FSC.csv')
@@ -75,17 +76,46 @@ def main():
             x_val[i] = int(f.split('_')[1])
             model_name.append('model'+str(i))
 
-            plt.figure()
-            plt.bar(x_val,resolution05[3,:],width=0.2, label='FBP')
-            plt.bar(x_val+0.2,resolution05[4,:],width=0.2, label='FBP undeformed')
-            plt.bar(x_val+0.4,resolution05[0,:],width=0.2,label='ours')
-            plt.bar(x_val+0.6,resolution05[5,:],width=0.2,label='FBP est deformation')
-            plt.xticks(x_val+0.3, model_name)
-            plt.ylabel('Resolution (1/pixel size)')
-            plt.legend()
-            plt.savefig(os.path.join(config.path_save,'evaluation','resolution05.pdf'))
-            plt.close()
-            
+
+        config.path_save_data = "./results/SNR_exp_"+str(config.volume_name)+"_size_"+str(config.n1)+"_Nangles_"+str(config.Nangles)+"/"
+        if not os.path.exists(config.path_save):
+            os.makedirs(config.path_save)
+        if not os.path.exists(config.path_save+"evaluation/"):
+            os.makedirs(config.path_save+"evaluation/")
+
+        plt.figure()
+        plt.bar(x_val,resolution05[3,:],width=0.2, label='FBP')
+        plt.bar(x_val+0.2,resolution05[4,:],width=0.2, label='FBP undeformed')
+        plt.bar(x_val+0.4,resolution05[0,:],width=0.2,label='ICETIDE')
+        plt.bar(x_val+0.6,resolution05[5,:],width=0.2,label='FBP est deformation')
+        plt.xticks(x_val+0.3, model_name)
+        plt.ylabel('Resolution (1/pixel size)')
+        plt.legend()
+        plt.savefig(os.path.join(config.path_save,'evaluation','resolution05.pdf'))
+        plt.close()        
+        
+        plt.figure()
+        plt.bar(x_val,resolution0143[3,:],width=0.2, label='FBP')
+        plt.bar(x_val+0.2,resolution0143[4,:],width=0.2, label='FBP undeformed')
+        plt.bar(x_val+0.4,resolution0143[0,:],width=0.2,label='ICETIDE')
+        plt.bar(x_val+0.6,resolution0143[5,:],width=0.2,label='FBP est deformation')
+        plt.xticks(x_val+0.3, model_name)
+        plt.ylabel('Resolution (1/pixel size)')
+        plt.legend()
+        plt.savefig(os.path.join(config.path_save,'evaluation','resolution0143.pdf'))
+
+        #save as csv file with header and SNR values as columns
+        resolution05 = np.vstack((model_name,resolution05))
+        resolution0143 = np.vstack((model_name,resolution0143))
+        header = ['ours','ETOMO','AreTomo','FBP','FBP_no_deformed','FBP_est_deformed']
+        header= 'MODEL_NAME'+','+','.join(header)
+        pd_resoluton05 = pd.DataFrame(resolution05.T,columns=header.split(','))
+        pd_resoluton0143 = pd.DataFrame(resolution0143.T,columns=header.split(','))
+        pd_resoluton05.to_csv(os.path.join(config.path_save,'evaluation','resolution05.csv'),index=False)
+        pd_resoluton0143.to_csv(os.path.join(config.path_save,'evaluation','resolution0143.csv'),index=False)
+
+
+        
 
 
 if __name__ == '__main__':
