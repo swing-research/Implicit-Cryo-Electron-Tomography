4b60d3f506ce80331cbedaac19cbbe957926e6e0
ValentinDebarnot <valentin.debarnot@gmail.com>
2024-01-22 08:32:37 +0100

    play with real

--- configs/real_data_tkiuv.py
+++ configs/real_data_tkiuv.py
@@ -146,12 +146,12 @@ def get_config():
     # params for the multi-resolution grids encoding
     config.encoding = ml_collections.ConfigDict()
     config.encoding.otype = 'Grid'
-    config.encoding.type = 'Hash'
-    config.encoding.n_levels = 8#
+    config.encoding.type = 'Dense'
+    config.encoding.n_levels = 3
     config.encoding.n_features_per_level = 4
     config.encoding.log2_hashmap_size = 22
-    config.encoding.base_resolution = 8
-    config.encoding.per_level_scale = 2#1.3
+    config.encoding.base_resolution = 128
+    config.encoding.per_level_scale = 2
     config.encoding.interpolation = 'Smoothstep'
     # params specific to Tiny cuda network
     config.network = ml_collections.ConfigDict()
