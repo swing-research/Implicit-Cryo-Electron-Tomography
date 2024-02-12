0d3d9fc55a417a333b67014db9d07ae045983c11
Valentin Debarnot <valentin.debarnot@gmail.com>
2024-02-12 15:39:49 +0100

    update comparison + get Fourier of slice

--- compare_results.py
+++ compare_results.py
@@ -1422,19 +1422,12 @@ def compare_results_real(config):
     # Define angles and X-ray transform
     angles = np.linspace(config.view_angle_min,config.view_angle_max,config.Nangles)
     operator_ET = ParallelBeamGeometry3DOpAngles_rectangular((config.n1,config.n2,config.n3), angles/180*np.pi, fact=1)
-
     projections_icetide = operator_ET(V_icetide_t).detach().cpu().numpy()
 
-
-    config.n1 = V_FBP_t.shape[0]
-    config.n2 = V_FBP_t.shape[1]
-    config.n3 = V_FBP_t.shape[2]
+    operator_ET = ParallelBeamGeometry3DOpAngles_rectangular((V_FBP_t.shape[0],V_FBP_t.shape[1],V_FBP_t.shape[2]), angles/180*np.pi, fact=1)
     projections_FBP = operator_ET(V_FBP_t).detach().cpu().numpy()
     projections_FBP_icetide = projections_noisy_undeformed.detach().cpu().numpy()
-
-    config.n1 = V_FBP_aretomo.shape[0]
-    config.n2 = V_FBP_aretomo.shape[1]
-    config.n3 = V_FBP_aretomo.shape[2]
+    operator_ET = ParallelBeamGeometry3DOpAngles_rectangular((V_FBP_aretomo.shape[0],V_FBP_aretomo.shape[1],V_FBP_aretomo.shape[2]), angles/180*np.pi, fact=1)
     if(eval_AreTomo):
         V_FBP_aretomo_t = torch.tensor(V_FBP_aretomo).to(device)
         projections_AreTomo = operator_ET(V_FBP_aretomo_t).detach().cpu().numpy()
