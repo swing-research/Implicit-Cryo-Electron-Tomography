2d45224c40b0505877f92f4208ef3bab06e36672
Valentin Debarnot <valentin.debarnot@gmail.com>
2024-05-24 08:35:59 +0200

    update install file

--- install_ICETIDE.sh
+++ install_ICETIDE.sh
@@ -6,6 +6,9 @@ pip install torch==2.0.1+cu117 torchvision==0.15.2+cu117 --extra-index-url https
 conda install -y -c "nvidia/label/cuda-11.7.1" cuda-toolkit
 pip install ninja git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
 pip install odl==0.7
+conda install -y numpy=1.23.4
+pip install bm3d
+conda install -y -c astra-toolbox astra-toolbox
 conda install -y ipython
 conda install -y scikit-image
 conda install -y -c jmcmurray os
@@ -15,3 +18,4 @@ pip install numexpr
 conda install -y -c conda-forge ml-collections
 conda install -y -c conda-forge ipdb
 conda install simpleitk::simpleitk
+conda install -y anaconda::pandas 
