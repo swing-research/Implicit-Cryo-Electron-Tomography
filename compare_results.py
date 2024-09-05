b222dd90822343f2fbb5b9c6172bb6df4df510c4
Valentin Debarnot <valentin.debarnot@gmail.com>
2024-09-06 01:56:21 +0200

    add sart+tv comparison

--- compare_results.py
+++ compare_results.py
@@ -262,7 +262,6 @@ def compare_results(config):
     V_FBP = V_FBP_t.detach().cpu().numpy()
     V_FBP_no_deformed = V_FBP_no_deformed_t.detach().cpu().numpy()
     proj_no_deformed = np.double(mrcfile.open(config.path_save_data+"projections_noisy_no_deformed.mrc").data)
-    import ipdb; ipdb.set_trace()
     V_no_deformed_sart_tv, _ = sart_update(V_FBP, proj_no_deformed, angles, lamb=config.lamb_sart,
                                      tau=config.tau_sart, nit=config.nit_sart, nit_tv=config.nit_tv_sart)
     tmp = V_no_deformed_sart_tv
@@ -278,6 +277,7 @@ def compare_results(config):
         plt.tight_layout(pad=1, w_pad=-1, h_pad=1)
         plt.savefig(os.path.join(config.path_save_data,'evaluation',"volumes",name+"_XYZ.png"))
     display_XYZ(tmp,name="SART_TV_no_deformed")
+    print('SART OVER #######################')
 
 
 
