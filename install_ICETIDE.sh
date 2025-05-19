9bb8dfbe7d67853436d08fb9d757f24a735f500f
Valentin Debarnot <valentin.debarnot@creatis.insa-lyon.fr>
2025-05-19 12:29:05 +0200

    update install files

--- install_ICETIDE.sh
+++ install_ICETIDE.sh
@@ -1,22 +1,16 @@
-conda create --name ice-tide -y python=3.8
-conda activate ice-tide
+conda create --name ice_tide -y python=3.8
+conda activate ice_tide
+
 sleep 10
 python -m pip install --upgrade pip
-pip install torch==2.0.1+cu117 torchvision==0.15.2+cu117 --extra-index-url https://download.pytorch.org/whl/cu117
-conda install -y -c "nvidia/label/cuda-11.7.1" cuda-toolkit
+conda install -y conda-forge::libmamba
+mamba install -y conda-forge::tomopy
+pip3 install torch torchvision torchaudio
 pip install ninja git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
-pip install odl==0.7
-conda install -y numpy=1.23.4
-pip install bm3d
-conda install -y -c astra-toolbox astra-toolbox
-conda install -y ipython
-conda install -y scikit-image
-conda install -y -c jmcmurray os
-conda install -y -c conda-forge mrcfile
-conda install -y matplotlib
-pip install numexpr
-conda install -y -c conda-forge ml-collections
-conda install -y -c conda-forge ipdb
-conda install simpleitk::simpleitk
-conda install -y anaconda::pandas
-conda install -y conda-forge::tomopy
+mamba install -y anaconda::ipython
+mamba install -y -c jmcmurray os
+mamba install -y -c conda-forge mrcfile
+mamba install -y matplotlib
+mamba install -y -c conda-forge ml-collections
+mamba install -y -c conda-forge ipdb
+mamba install -y conda-forge::glob2
--- install_ICETIDE_mamba.sh
+++ install_ICETIDE_mamba.sh
@@ -1,24 +1,15 @@
-conda create --name ice_tide_test -y python=3.8
-conda activate ice_tide_test
+conda create --name ice_tide -y python=3.8
+conda activate ice_tide
 
 sleep 10
 python -m pip install --upgrade pip
-conda install -y conda-forge::libmamba
-mamba install -y conda-forge::tomopy
+conda install -y conda-forge::tomopy
 pip3 install torch torchvision torchaudio
 pip install ninja git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
-mamba install -y anaconda::ipython
-mamba install -y -c jmcmurray os
-mamba install -y -c conda-forge mrcfile
-mamba install -y matplotlib
-mamba install -y -c conda-forge ml-collections
-mamba install -y -c conda-forge ipdb
-mamba install -y conda-forge::glob2
-
-##glob
-#
-#mamba install simpleitk::simpleitk
-#mamba install -y anaconda::pandas
-#pip install numexpr
-#mamba install -y -c astra-toolbox astra-toolbox
-#pip install bm3d
+conda install -y anaconda::ipython
+conda install -y -c jmcmurray os
+conda install -y -c conda-forge mrcfile
+conda install -y matplotlib
+conda install -y -c conda-forge ml-collections
+conda install -y -c conda-forge ipdb
+conda install -y conda-forge::glob2
